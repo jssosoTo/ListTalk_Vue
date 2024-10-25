5c92fd5066bebfedab6ff3116d016fc986b90056
P0PC0RN <2982538245@qq.com>
2024-10-25 21:30:35 +0800

    ✨ feat(全局公共组件): 分割线组件

---
+++ src/components/Divider.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { AiFillCodeSandboxCircle } from 'vue-icons-plus/ai'
+</script>
+
+<template>
+  <div class="flex items-center gap-4 justify-between divider">
+    <div></div>
+    <AiFillCodeSandboxCircle />
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.divider svg {
+  width: 0.9rem;
+  color: var(--shade-border-color);
+}
+
+.divider div {
+  flex: 1;
+  height: 1px;
+  background-color: var(--shade-border-color);
+}
+</style>
