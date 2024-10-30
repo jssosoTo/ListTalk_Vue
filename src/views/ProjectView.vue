628b52503b9fbd3976f89ae835ea52fab315b6ad
P0PC0RN <2982538245@qq.com>
2024-10-30 21:35:36 +0800

    ✨ feat(我的项目页面): 页面开发

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,7 +1,69 @@
 <script setup lang="ts">
+import ListContainer from '@/components/ListContainer.vue'
+import TaskItem from '@/components/TaskItem.vue'
+import { useAlertStore } from '@/stores/alert'
+import { useReloadStore } from '@/stores/reload'
+import { computed, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+const id = ref(route.params.id)
+const lists = ref(JSON.parse(localStorage.getItem('allLists') || '[]'))
+const reloadStore = useReloadStore()
+const alertStore = useAlertStore()
+
+function reload() {
+  lists.value = JSON.parse(localStorage.getItem('allLists') || '[]')
+}
+
+function checkedItem(id: number) {
+  const newLists = lists.value.slice()
+  newLists.find(item => item.id === id).checked = true
+  localStorage.setItem('allLists', JSON.stringify(newLists))
+  lists.value = newLists
+  alertStore.openAlert('1个任务已完成', id)
+}
+
+reloadStore.changeReload(reload)
+
+const sortLists = computed(() => {
+  const premierNum = {
+    first: 1,
+    second: 2,
+    third: 3,
+    forth: 4,
+  }
+  return lists.value
+    .slice()
+    .filter(item => !item.checked && +id.value === +item.type.value)
+    .sort((a, b) => premierNum[a.premier] - premierNum[b.premier])
+})
+
+watch(
+  () => route.params.id,
+  () => {
+    id.value = route.params.id
+  },
+)
 </script>
 
-<template>Projects: {{ route.params.id }}</template>
+<template :key="id">
+  <ListContainer
+    title="今天"
+    :task-num="sortLists.length"
+    @reload="reload"
+    :is-task-num-show="true"
+  >
+    <TaskItem
+      v-for="list in sortLists"
+      @checkedItem="checkedItem"
+      :key="list.id"
+      :id="list.id"
+      :title="list.title"
+      :desc="list.desc"
+      :date="list.date"
+      :premier="list.premier"
+      :type="list.type"
+    />
+  </ListContainer>
+</template>
