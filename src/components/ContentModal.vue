37cdb1803c5e2f96684eb23bdb5e433de3eb07a6
P0PC0RN <2982538245@qq.com>
2024-11-01 01:35:11 +0800

    ✨ feat(小弹窗组件): 新增可以设置class以及样式微调整

--- src/components/ContentModal.vue
+++ src/components/ContentModal.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+</script>
 
 <template>
   <div class="relative">
     <slot name="button"></slot>
-    <div class="absolute -top-6 translate-y-1/4 left-1/2 -translate-x-1/2">
+    <div
+      class="modal absolute translate-y-2 left-1/2 -translate-x-1/2"
+      :class="$attrs.class"
+    >
       <slot name="content"></slot>
     </div>
   </div>
 </template>
+
+<style scoped>
+.modal {
+  top: 100%;
+}
+</style>
