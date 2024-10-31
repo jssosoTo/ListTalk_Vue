056d7ff7b7595d2f2d25c9c662b2f19e26c3842c
P0PC0RN <2982538245@qq.com>
2024-10-31 23:32:45 +0800

    ✨ feat(回收站): 页面样式开发

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectView from '@/views/ProjectView.vue'
+import Recycle from '@/views/Recycle.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/today',
       name: 'Today',
       component: HomeView,
+    },
+    {
+      path: '/recycle',
+      name: 'recycle',
+      component: Recycle,
     },
     {
       path: '/projects/:id',
---
+++ src/views/Recycle.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { FcFullTrash } from 'vue-icons-plus/fc'
+import { PiTrashSimple } from 'vue-icons-plus/pi'
+import { RiArrowGoBackLine } from 'vue-icons-plus/ri'
+</script>
+
+<template>
+  <div class="px-24 pt-8 pb-4 h-full flex flex-col overflow-y-auto">
+    <h1 class="text-xl">已确认的任务</h1>
+    <div class="title flex items-center justify-between mt-4">
+      <h2>系统会在 30 天后删除这些任务</h2>
+      <div class="btns">
+        <button class="flex items-center gap-2 fnBtn">
+          <FcFullTrash />
+          清空回收站
+        </button>
+      </div>
+    </div>
+    <table class="flex-1 w-full text-left overflow-y-auto relative">
+      <thead class="sticky -top-8 header">
+        <th>日期</th>
+        <th>标题</th>
+        <th>完成日期</th>
+        <th>操作</th>
+      </thead>
+      <tbody>
+        <tr v-for="n in 20">
+          <td>2024年11月4日</td>
+          <td>是大大啊倒是</td>
+          <td>2024年11月1日</td>
+          <td class="flex items-center gap-4 options">
+            <button>
+              <RiArrowGoBackLine />
+            </button>
+            <button>
+              <PiTrashSimple />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  z-index: 1;
+  background-color: var(--bg);
+}
+
+.fnBtn {
+  padding: 0.4rem 1rem;
+  border-radius: 5px;
+}
+
+.fnBtn:hover {
+  background-color: var(--btn-color);
+  color: var(--btn-text);
+}
+
+th,
+td {
+  padding: 0.8rem 0.5rem;
+}
+
+td {
+  border-top: 1px solid var(--text);
+}
+
+table {
+  border: 1px solid var(--text);
+  border-radius: 10px;
+  border-collapse: separate;
+}
+
+.options button {
+  position: relative;
+}
+
+.options button:hover::after {
+  content: '';
+  position: absolute;
+  width: 2.4rem;
+  height: 2.4rem;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--shade-bg);
+}
+</style>
