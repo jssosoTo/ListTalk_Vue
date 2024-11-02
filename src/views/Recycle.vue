35bf9dd06ab05adaa2b2bf41a7f848c7f0d67e16
P0PC0RN <2982538245@qq.com>
2024-11-02 14:09:14 +0800

    ✨ feat(回收站页面): 引入气泡确认框组件

--- src/views/Recycle.vue
+++ src/views/Recycle.vue
@@ -1,48 +1,23 @@
 <script setup lang="ts">
-import ContentModal from '@/components/ContentModal.vue'
-import { ref } from 'vue'
+import PopConfirm from '@/components/PopConfirm.vue'
 import { FcFullTrash } from 'vue-icons-plus/fc'
 import { PiTrashSimple } from 'vue-icons-plus/pi'
 import { RiArrowGoBackLine } from 'vue-icons-plus/ri'
-
-const isTrashModalOpen = ref(false)
 </script>
 <!-- @click.capture="isTrashModalOpen = false" -->
+
 <template>
   <div class="px-24 pt-8 pb-4 h-full flex flex-col overflow-y-auto">
     <h1 class="text-xl">已确认的任务</h1>
     <div class="title flex items-center justify-between mt-4">
       <h2>系统会在 30 天后删除这些任务</h2>
       <div class="btns">
-        <ContentModal class="z-10">
-          <template #button>
-            <button
-              class="flex items-center gap-2 fnBtn"
-              @click="isTrashModalOpen = true"
-            >
-              <FcFullTrash />
-              清空回收站
-            </button>
-          </template>
-          <template #content v-if="isTrashModalOpen">
-            <div class="w-max translate-y-0 px-4 py-2 rounded trash_modal">
-              <h4 class="mb-2 text-lg">确认清空回收站吗？</h4>
-              <div class="flex justify-end gap-1">
-                <button class="cancel_btn" @click="isTrashModalOpen = false">
-                  取消
-                </button>
-                <button
-                  type="submit"
-                  class="submit_btn"
-                  @click="console.log('save')"
-                >
-                  保存
-                </button>
-              </div>
-              <div class="arrow-circle"></div>
-            </div>
-          </template>
-        </ContentModal>
+        <PopConfirm title="确认清空回收站吗" @on-confirm="console.log('save')">
+          <button class="flex items-center gap-2 fnBtn">
+            <FcFullTrash />
+            清空回收站
+          </button>
+        </PopConfirm>
       </div>
     </div>
     <table class="flex-1 w-full text-left overflow-y-auto relative">
@@ -118,19 +93,5 @@ table {
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: var(--shade-bg);
-}
-
-.trash_modal {
-  position: relative;
-  background-color: var(--modal-bg);
-}
-
-.arrow-circle {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateY(-100%);
-  border: 0.6rem solid transparent;
-  border-bottom-color: var(--modal-bg);
 }
 </style>
