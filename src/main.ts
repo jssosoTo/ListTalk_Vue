d7952d4865163393a48aa4bebd1dafec351b7ec3
P0PC0RN <2982538245@qq.com>
2024-11-02 15:17:16 +0800

    🐞 fix(全局): 删除冗余注册组件

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import Mask from './Mask.vue'
-import AlertMask from './AlertMask.vue'
 import router from './router'
 
 const app = createApp(App)
-const mask = createApp(Mask)
-const alertMask = createApp(AlertMask)
 
 app.use(createPinia())
 app.use(router)
 
-mask.use(createPinia())
-alertMask.use(createPinia())
-
-mask.mount('#mask')
 app.mount('#app')
-alertMask.mount('#alert')
